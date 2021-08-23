45fab888d831a68796569c22f482fdf854a6d371
Lily Meng <lilyxmeng13@gmail.com>
2021-08-22 21:01:36 -0400

    commit

--- "Fun" Math/postfix.cpp
+++ "Fun" Math/postfix.cpp
@@ -4,21 +4,32 @@ using namespace std;
 string s;
 stack<double> stk;
 
-int main() {
-    while (cin >> s) {
-        if (isdigit(s[0])) {
+int main()
+{
+    while (cin >> s)
+    {
+        if (isdigit(s[0]))
+        {
             stk.push(stod(s));
-        } else {
+        }
+        else
+        {
             double op2 = stk.top();
             stk.pop();
             double op1 = stk.top();
             stk.pop();
-            if (s == "+") stk.push(op1 + op2);
-            if (s == "-") stk.push(op1 - op2);
-            if (s == "*") stk.push(op1 * op2);
-            if (s == "/") stk.push(op1 / op2);
-            if (s == "%") stk.push(fmod(op1, op2));
-            if (s == "^") stk.push(pow(op1, op2));
+            if (s == "+")
+                stk.push(op1 + op2);
+            if (s == "-")
+                stk.push(op1 - op2);
+            if (s == "*")
+                stk.push(op1 * op2);
+            if (s == "/")
+                stk.push(op1 / op2);
+            if (s == "%")
+                stk.push(fmod(op1, op2));
+            if (s == "^")
+                stk.push(pow(op1, op2));
         }
     }
     cout << stk.top() << endl;
